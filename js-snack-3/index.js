3506acf0e8b229120998153a41b98f6fee7137ea
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-18 11:14:01 +0200

    feat: snack-3

---
+++ js-snack-3/index.js
@@ -0,0 +1,15 @@
+const str = "racecar";
+const str2 = "hello";
+
+
+function isPalindrome (str) {
+    let reversed = str.split('').reverse().join('');
+    
+    if (str === reversed) {
+        return console.log(true);
+    } else {
+        return console.log(false);
+    }
+}
+isPalindrome(str);
+isPalindrome(str2);
