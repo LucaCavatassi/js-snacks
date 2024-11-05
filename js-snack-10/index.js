bb3d9ffe000b29c7be967e7caa92f4c44fdf5f8b
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-05 16:43:49 +0100

    feat: js-snack-10

--- js-snack-10/index.js
+++ js-snack-10/index.js
@@ -1,16 +1,6 @@
-function removeDuplicates(arr) {
-    let result = [];
-    arr.reduce((prev, curr) => {
-        if (prev !== curr) {
-            result.push(prev);
-        }
-        return curr; // Return the current value for the next iteration
-    });
-    // Push the last element if it is not already in the result
-    if (result[result.length - 1] !== arr[arr.length - 1]) {
-        result.push(arr[arr.length - 1]);
-    }
-    return result;
+function reverseString(str) {
+    return str.split('').reverse().join('');
 }
 
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+// Example usage:
+console.log(reverseString("hello")); // "olleh"
