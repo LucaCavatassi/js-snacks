94a5081b4cca52c2c21492733d38fcde3872c288
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-14 16:36:30 +0100

    feat: OOP 2

--- js-OOP/index.js
+++ js-OOP/index.js
@@ -19,5 +19,26 @@ class Persona {
 const persona1 = new Persona("Luca", 29);
 const persona2 = new Persona("Francesca", 31);
 
-console.log(persona1.saluta());
-console.log(persona2.saluta());
+// console.log(persona1.saluta());
+// console.log(persona2.saluta());
+
+// Esercizio 2: Ereditarietà
+
+// Estendi la classe Persona creando una sottoclasse Studente con:
+// 	•	Un costruttore che accetta anche un parametro corso.
+// 	•	Un metodo infoCorso() che restituisce "Sto studiando <corso>."
+
+class Studente extends Persona {
+    constructor(nome, eta, corso){
+        super(nome, eta);
+        this.corso = corso
+    }
+
+    infoCorso() {
+        return `Sto studiando ${this.corso}.`
+    }
+}
+
+const studente1 = new Studente("Anna", 22, "Informatica");
+console.log(studente1.saluta()); // "Ciao, mi chiamo Anna e ho 22 anni."
+console.log(studente1.infoCorso()); // "Sto studiando Informatica."
