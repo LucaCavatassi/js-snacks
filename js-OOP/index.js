3dfcb51fc8207f1345b3d2d92aed23f5a29ccdd4
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-14 16:49:05 +0100

    feat: OOP 4

--- js-OOP/index.js
+++ js-OOP/index.js
@@ -5,28 +5,28 @@
 // 	•	Un metodo saluta() che restituisce una stringa: "Ciao, mi chiamo <nome> e ho <eta> anni."
 
 
-class Persona {
-    constructor(nome, eta) {
-        this._eta = eta;
-        this.nome = nome;
-    }
+// class Persona {
+//     constructor(nome, eta) {
+//         this._eta = eta;
+//         this.nome = nome;
+//     }
 
-    saluta() {
-        return `Ciao mi chiamo ${this.nome} e ho ${this._eta} anni.`
-    }
+//     saluta() {
+//         return `Ciao mi chiamo ${this.nome} e ho ${this._eta} anni.`
+//     }
 
-    get eta() {
-        return this._eta;
-    }
+//     get eta() {
+//         return this._eta;
+//     }
 
-    set eta(eta) {
-        if (eta <= 0) {
-            console.log('Eta deve essere positiva');
-        } else {
-            this._eta = eta
-        }
-    } 
-}
+//     set eta(eta) {
+//         if (eta <= 0) {
+//             console.log('Eta deve essere positiva');
+//         } else {
+//             this._eta = eta
+//         }
+//     } 
+// }
 
 // const persona1 = new Persona("Luca", 29);
 // const persona2 = new Persona("Francesca", 31);
@@ -40,16 +40,16 @@ class Persona {
 // 	•	Un costruttore che accetta anche un parametro corso.
 // 	•	Un metodo infoCorso() che restituisce "Sto studiando <corso>."
 
-class Studente extends Persona {
-    constructor(nome, eta, corso){
-        super(nome, eta);
-        this.corso = corso
-    }
+// class Studente extends Persona {
+//     constructor(nome, eta, corso){
+//         super(nome, eta);
+//         this.corso = corso
+//     }
 
-    infoCorso() {
-        return `Sto studiando ${this.corso}.`
-    }
-}
+//     infoCorso() {
+//         return `Sto studiando ${this.corso}.`
+//     }
+// }
 
 // const studente1 = new Studente("Anna", 22, "Informatica");
 // console.log(studente1.saluta()); // "Ciao, mi chiamo Anna e ho 22 anni."
@@ -58,12 +58,37 @@ class Studente extends Persona {
 // Esercizio 3: Incapsulamento con Getter e Setter
 // Modifica la classe Persona per includere una proprietà privata _eta e usa getter e setter per accedere e modificare eta, assicurandoti che l’età possa essere impostata solo se positiva.
 
-const persona2 = new Persona("Marco", 30);
-console.log(persona2.eta); // 30
+// const persona2 = new Persona("Marco", 30);
+// console.log(persona2.eta); // 30
 
-persona2.eta = -5; // Non dovrebbe cambiare l'età
-console.log(persona2.eta); // 30
+// persona2.eta = -5; // Non dovrebbe cambiare l'età
+// console.log(persona2.eta); // 30
 
-persona2.eta = 32; // Dovrebbe aggiornare l'età
-console.log(persona2.eta); // 32
-console.log(persona2.saluta());
+// persona2.eta = 32; // Dovrebbe aggiornare l'età
+// console.log(persona2.eta); // 32
+// console.log(persona2.saluta());
+
+// Esercizio 4: Polimorfismo
+// Crea una classe Animale con un metodo verso() che restituisce "Un suono generico". 
+// Crea poi due classi Cane e Gatto che estendono Animale e sovrascrivono il metodo verso() per restituire rispettivamente "Woof!" e "Miao!".
+
+class Animale{
+    verso() {
+        return 'Un suono generico';
+    }
+}
+
+class Cane extends Animale {
+    verso() {
+        return 'Woof!'
+    }
+}
+
+class Gatto extends Animale {
+    verso() {
+        return 'Miao!'
+    }
+}
+
+const animali = [new Cane(), new Gatto()];
+animali.forEach(animale => console.log(animale.verso()));
