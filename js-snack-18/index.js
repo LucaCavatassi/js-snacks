be488146a8e0edf1c803c6c26bd4586e5eb4be7e
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-11-14 10:34:48 +0100

    feat: js-snack-18

--- js-snack-18/index.js
+++ js-snack-18/index.js
@@ -1,19 +1,24 @@
 // 5.	Filtrare valori duplicati
 // Scrivi una funzione rimuoviDuplicati che accetta un array e restituisce un nuovo array senza i valori duplicati.
 
-function rimuoviDuplicati(arr) {
-    let result = [];
-    arr.reduce((prev, curr) => {
-        if (prev !== curr) {
-            result.push(prev)
-        }
-        return curr
-    })
+// function rimuoviDuplicati(arr) {
+//     let result = [];
+//     arr.reduce((prev, curr) => {
+//         if (prev !== curr) {
+//             result.push(prev)
+//         }
+//         return curr
+//     })
 
-    if (arr.length > 0) {
-        result.push(arr[arr.length - 1]);
-    }
-    
-    return result
-}
+//     if (arr.length > 0) {
+//         result.push(arr[arr.length - 1]);
+//     }
+
+//     return result
+// }
+// console.log(rimuoviDuplicati([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+
+function rimuoviDuplicati(arr){
+    return [...new Set(arr)]
+} 
 console.log(rimuoviDuplicati([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
