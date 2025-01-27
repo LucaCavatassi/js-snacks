b9d95d72cd4c8c8e3fd85c52f6f28c6e608be7e2
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-27 17:41:17 +0100

    feat: snack-3

--- js-snack-22/index.js
+++ js-snack-22/index.js
@@ -19,12 +19,22 @@
 
 
 
-function getRandomColor() {
-    return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
+// function getRandomColor() {
+//     return "#" + Math.floor(Math.random() * 16777215).toString(16);
+// }
 
-const btn = document.getElementById("changeColor");
-btn.addEventListener("click", function() {
-    document.body.style.backgroundColor = getRandomColor();
-});
+// const btn = document.getElementById("changeColor");
+// btn.addEventListener("click", function() {
+//     document.body.style.backgroundColor = getRandomColor();
+// });
   // Aggiungi l'event listener per cambiare colore allo sfondo
+
+const user = {
+    name: "Luca",
+    age: 30,
+    city: "Milano"
+};
+
+function formatData(user) {
+    return `${user.name} ha ${user.age} anni e vive a ${user.city}.`
+}
