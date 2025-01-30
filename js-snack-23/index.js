4ec4904ef5177ca8fcb9c0e99feadefdf6ba69cf
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-01-30 10:29:28 +0100

    feat :kata 1

---
+++ js-snack-23/index.js
@@ -0,0 +1,54 @@
+const gridPosition = (name, position) => {
+  const rows = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [10,11,12],
+        [13,14,15],
+        [16,17,18],
+        [19,20,21]
+    ];
+
+    let rowIndex = rows.findIndex(row => row.includes(position));
+
+    if (rowIndex === -1) {
+        console.log("Position not available.");
+        return;
+    }
+
+    let colIndex = rows[rowIndex].indexOf(position);
+    
+    let firstWord = '' 
+    let secondWord = '';
+    let result = ''
+
+    if (colIndex === 0 && rowIndex === 0) {
+        firstWord = 'pole position'
+    } else if (colIndex === 0) {
+        firstWord = 'top'
+    } else if (colIndex === 1) {
+        firstWord = 'middle'
+    } else if (colIndex === 2) {
+        firstWord = 'bottom'
+    }
+
+    if (rowIndex === 0) {
+        secondWord = 'front'
+    } else if (rowIndex === rows.length - 1) {
+        secondWord = 'last'
+    } else if (rowIndex + 1 === 2) {
+        secondWord = '2nd'
+    } else if (rowIndex + 1 === 3) {
+        secondWord = '3rd'
+    } else if (rowIndex + 1 > 3) {
+        secondWord = `${rowIndex + 1}th`
+    }
+
+    if (firstWord === 'pole position') {
+        result = `${name} starts from ${firstWord}`
+    } else {
+        result = `${name} starts from the ${firstWord} of the ${secondWord} row`
+    }
+
+    return result
+}
